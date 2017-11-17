0b4d5007110f8ba1f38cf0324dd685b50c807a00
Jessica Gross <jessicasarahgross@gmail.com>
2017-11-17 18:44:21 -0500

    adds js nav

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var navToggle = document.querySelector('#main-nav-toggle');
+var navList = document.querySelector('#main-nav-list');
+
+
+
+//Always check to see if elements exist before writing js involving those elements by console.log a message
+if (navToggle && navList){
+  navToggle.addEventListener('click', function (){
+    // This checks to seee if the element has the class of close on it
+    if (navToggle.classList.contains('close')) {
+      navToggle.classList.remove("close");
+      navList.classList.remove("open");
+    } else {
+      navToggle.classList.add('close');
+      navList.classList.add('open');
+    }
+  });
+}
