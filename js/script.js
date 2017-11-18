b1dacbbb47ca9270c763b13aa2738087efec9c23
Jessica Gross <jessicasarahgross@gmail.com>
2017-11-17 19:14:08 -0500

    adds event

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ var navList = document.querySelector('#main-nav-list');
 
 //Always check to see if elements exist before writing js involving those elements by console.log a message
 if (navToggle && navList){
-  navToggle.addEventListener('click', function (){
+  navToggle.addEventListener('click', function (event){
     // This checks to seee if the element has the class of close on it
+    console.log(event);
     if (navToggle.classList.contains('close')) {
       navToggle.classList.remove("close");
       navList.classList.remove("open");
